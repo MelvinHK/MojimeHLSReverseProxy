0af8069ae41528734a97e552ce1c9f64c0a30bd2
Melvin <melvinhkusuma@gmail.com>
2024-12-19 00:10:03 +1100

    changed base route message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 
-app.get("/", (_req, res) => res.send("Running reverse proxy."));
+app.get("/", (_req, res) => res.send("This is a reverse proxy for `mojime.vercel.app`."));
 
 app.use('/proxy/:url(*)', async (req, res) => {
     try {
